1c2fcb9f1e0f0f2f20446cf4563b40365b8d983b
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2023-12-19 14:47:14 -0800

    Update Person.java

--- 0. Notes/Person.java
+++ 0. Notes/Person.java
@@ -1,6 +1,4 @@
 public class Person {
-    public static String species = "Human";
-    private static int numHearts = 1;
     private String name;
     private int age;
 
@@ -12,10 +10,6 @@ public class Person {
     public Person(String n, int a) {
         name = n;
         age = a;
-    }
-
-    public static int getNumHearts() {
-        return numHearts;
     }
 
     public String getName() {
@@ -32,34 +26,5 @@ public class Person {
 
     public void setAge(int age) {
         age = age;
-    }
-
-    public String toString() {
-        return hearts() + name + " is a " + age + " year old " + species;
-    }
-
-    public static String hearts() {
-        return "All " + species + " have " + numHearts + " heart. ";
-    }
-
-    public static void howManyHearts(int h) {
-        if (h == numHearts) {
-            System.out.print("Correct! ");
-        } else {
-            System.out.print("Incorrect! ");
-        }
-        System.out.println(hearts());
-    }
-
-    public void copyName(Person p) {
-        name = p.name;
-    }
-
-    public int getYearBorn() {
-        return 2023 - age;
-    }
-    
-    public void sayYearBorn() {
-        System.out.println("I was born in " + getYearBorn());
     }
 }
